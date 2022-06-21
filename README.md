5dea72801f4b80649f0af34409bc306871f35355
mskian <veerfun23@gmail.com>
2022-06-21 21:14:35 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Copied Kavithai
+
+Copied Kavithai - Copied Kavithai from Tamil Kavithai Website's and Tamil Quotes Blog.
+
+> Tamil Kavithai Database
+
+Built using Deno Blog Generator - <https://github.com/denoland/deno_blog>
+
+## LICENSE
+
+MIT
---
+++ main.ts
@@ -0,0 +1,13 @@
+import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+blog({
+  title: "Copied Kavithai - Tamil Kavithai Database",
+  description: "Copied Kavithai - Tamil Kavithai Database - Copied Kavithai from Tamil Kavithai Website's and Tamil Quotes Blog.",
+  avatar: "./cloud.png",
+  avatarClass: "full",
+  author: "Copy Kavithai",
+  background: "#ecf0f1",
+  links: [
+    { title: "GitHub", url: "https://github.com/mskian/copied-kavithai" },
+  ],
+});
