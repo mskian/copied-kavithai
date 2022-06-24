7f57d84a921911eff142f60cf86976ba550e11cc
mskian <veerfun23@gmail.com>
2022-06-24 18:29:40 +0530

    remove img

--- main.ts
+++ main.ts
@@ -4,8 +4,6 @@ blog({
   title: "Copied Kavithai - Tamil Kavithai Database",
   description:
     "Copied Kavithai - Tamil Kavithai Database - Copied Kavithai from Tamil Kavithai Website's and Tamil Quotes Blog.",
-  avatar: "./cloud.png",
-  avatarClass: "full",
   author: "Copy Kavithai",
   background: "#ecf0f1",
   links: [
