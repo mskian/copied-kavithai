6a1e667e54a5ae6957031e2e05248860a3c867a5
mskian <veerfun23@gmail.com>
2022-08-01 19:40:39 +0530

    small changes

--- tag.tmpl.js
+++ tag.tmpl.js
@@ -4,7 +4,7 @@ export default function* ({ search }) {
   for (const tag of search.tags()) {
     yield {
       url: `/tags/${tag}/`,
-      title: `Tagged “${tag}”`,
+      title: `Tagged "${tag}"`,
       type: "tag",
       tag,
     };
