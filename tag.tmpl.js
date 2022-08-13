84f635f502ef7069337e24dd3e3d24c266239bb0
mskian <10300271+mskian@users.noreply.github.com>
2022-08-13 20:39:51 +0530

    Update post and slugify tags via slugify helper

--- tag.tmpl.js
+++ tag.tmpl.js
@@ -1,9 +1,9 @@
 export const layout = "layouts/tag.njk";
 
-export default function* ({ search }) {
+export default function* ({ search }, { slugify }) {
   for (const tag of search.tags()) {
     yield {
-      url: `/tag/${tag.toLowerCase().replaceAll(" ", "-")}/`,
+      url: slugify(`/tag/${tag}/`),
       title: `Tagged - ${tag}`,
       description: `Tagged - ${tag} - Tamil Copied Kavithai - Tamil Kavithai Site - Kavithai for Social Status and Stories.`,
       type: "tag",
