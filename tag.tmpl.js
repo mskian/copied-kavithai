897b9cc97ba3559513d915cc9c710566096bb6c6
mskian <10300271+mskian@users.noreply.github.com>
2022-08-15 22:26:56 +0530

    Update lume and it fixes tag slugify issue

--- tag.tmpl.js
+++ tag.tmpl.js
@@ -1,11 +1,11 @@
 export const layout = "layouts/tag.njk";
 
-export default function* ({ search }, { slugify }) {
+export default function* ({ search }) {
   for (const tag of search.tags()) {
     yield {
-      url: slugify(`/tag/${tag}/`),
+      url: `/tag/${tag}/`,
       title: `Tagged - ${tag}`,
-      description: `Tagged - ${tag} - Tamil Copied Kavithai - Tamil Kavithai Site - Kavithai for Social Status and Stories.`,
+      description: `Tagged - ${tag} - Tamil Copied Kavithai Database - Tamil Kavithai Site - Kavithai for Social Status and Stories.`,
       type: "tag",
       tag,
     };
